f48f76fe667760a0de6e1f4574c69acf150534ce
Your Name <your.email@example.com>
2026-04-20 23:00:44 +0530

    Fix NameError - add get_object_or_404 to imports in views.py

--- blood_requests_app/views.py
+++ blood_requests_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.utils import timezone
 import logging
